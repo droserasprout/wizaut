eb85720a2225a10ebdda4b8ff06c3fe9c0fa6566
Lev Gorodetskiy <github@droserasprout.space>
2024-01-03 14:45:31 -0300

    green

--- src/wizaut/api.py
+++ src/wizaut/api.py
@@ -17,11 +17,10 @@ config = YAML().load(config_path.read_text())
 
 KNOWN_BULBS = {k.replace(':', ''): v for k, v in config['devices'].items()}
 
-_discovered_lights = []
+_discovered_lights: list[wl.wizlight] = []
 
 
-
-async def _get_lights():
+async def _get_lights() -> list[wl.wizlight]:
     global _discovered_lights
     if not _discovered_lights:
         _discovered_lights = await wl.discovery.discover_lights(
@@ -32,19 +31,17 @@ async def _get_lights():
         await _light.updateState()
     return _discovered_lights
 
-async def _get_light(name):
-    light = wl.wizlight(name)
-    await light.updateState()
-    return light
 
 templates = Jinja2Templates(directory=Path(__file__).parent.joinpath('templates'))
 app = FastAPI()
 app.mount('/static', StaticFiles(directory=Path(__file__).parent.joinpath('static')), name='static')
 
+
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request) -> Response:
     data = {'page': 'Home page'}
     return templates.TemplateResponse('index.j2', {'request': request, 'data': data})
+
 
 class System(BaseModel):
     system_name: str
@@ -54,20 +51,21 @@ class System(BaseModel):
 async def get_lights(request: Request) -> Response:
     res = []
 
-
     lights = await _get_lights()
     for light in lights:
         await light.updateState()
         name = KNOWN_BULBS.get(light.mac) or light.mac
         state = light.state.get_state()
-        res.append({
-            'name': name,
-            'ip': light.ip,
-            'mac': light.mac,
-            'status': state,
-            'brightness': light.state.get_brightness() if state else 0,
-            'colortemp': light.state.get_colortemp(),
-        })
+        res.append(
+            {
+                'name': name,
+                'ip': light.ip,
+                'mac': light.mac,
+                'status': state,
+                'brightness': light.state.get_brightness() if state else 0,
+                'colortemp': light.state.get_colortemp(),
+            }
+        )
 
     return templates.TemplateResponse('lights.j2', {'request': request, 'lights': res})
 
@@ -76,6 +74,7 @@ async def get_lights(request: Request) -> Response:
 async def reset_lights(request: Request) -> Response:
     global _discovered_lights
     _discovered_lights = []
+    return Response()
 
 
 @app.get('/lights/off', response_class=HTMLResponse)
@@ -83,6 +82,7 @@ async def lights_off(request: Request) -> Response:
     for light in await _get_lights():
         await light.turn_off()
     return await get_lights(request)
+
 
 @app.get('/lights/on', response_class=HTMLResponse)
 async def lights_on(request: Request) -> Response:
@@ -92,8 +92,7 @@ async def lights_on(request: Request) -> Response:
 
 
 @app.get('/lights/{name}/flip', response_class=HTMLResponse)
-async def light_flip(request: Request) -> str:
-
+async def light_flip(request: Request) -> Response:
     import pywizlight as wl
 
     name = request.path_params['name']
@@ -107,7 +106,7 @@ async def light_flip(request: Request) -> str:
 @app.post('/lights/{name}/brightness', response_class=HTMLResponse)
 async def light_brightness(request: Request) -> None:
     name = request.path_params['name']
-    value = int((await request.form())['value'])
+    value = int((await request.form())['value'])  # type: ignore[arg-type]
 
     light = wl.wizlight(name)
     await light.updateState()
@@ -118,6 +117,7 @@ async def light_brightness(request: Request) -> None:
     else:
         await asyncio.wait_for(light.turn_off(), timeout)
 
+
 @app.post('/lights/{name}/warmth', response_class=HTMLResponse)
 async def light_warmth(request: Request) -> None:
     name = request.path_params['name']
@@ -126,4 +126,4 @@ async def light_warmth(request: Request) -> None:
     light = wl.wizlight(name)
     await light.updateState()
     timeout = config['timeout']
-    await asyncio.wait_for(light.turn_on(wl.PilotBuilder(colortemp=int(value))), timeout)
+    await asyncio.wait_for(light.turn_on(wl.PilotBuilder(colortemp=int(value))), timeout)  # type: ignore[arg-type]
