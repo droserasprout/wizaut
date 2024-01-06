000c1c7cbc31603c0fb965c7b6b4f21ce0230dd6
Lev Gorodetskiy <github@droserasprout.space>
2024-01-06 11:43:07 -0300

    more refactoring

--- src/wizaut/__init__.py
+++ src/wizaut/__init__.py
@@ -1,5 +1,18 @@
-from typing import TypedDict
+import asyncio
+from contextlib import suppress
 
 
-class Config(TypedDict):
-    ...
+def run() -> None:
+    import uvicorn
+
+    from wizaut.api import create_app
+    from wizaut.config import WizautConfig
+
+    config = WizautConfig.discover()
+
+    with suppress(KeyboardInterrupt, asyncio.CancelledError):
+        uvicorn.run(
+            create_app(config),
+            host=config.host,
+            port=config.port,
+        )
--- src/wizaut/api.py
+++ src/wizaut/api.py
@@ -10,114 +10,97 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from wizaut.config import WizautConfig
-
-config = WizautConfig.load(Path(__file__).parent.parent.parent / 'wizaut.yaml')
-
-KNOWN_BULBS = {k.replace(':', ''): v for k, v in config.devices.items()}
-
-_discovered_lights: list[wl.wizlight] = []
+from wizaut.wiz import WizManager
 
 
-async def _get_lights() -> list[wl.wizlight]:
-    global _discovered_lights
-    if not _discovered_lights:
-        _discovered_lights = await wl.discovery.discover_lights(
-            broadcast_space=config.broadcast,
-            wait_time=int(config.timeout),
-        )
-    for _light in _discovered_lights:
-        await _light.updateState()
-    return _discovered_lights
+def create_app(config: WizautConfig) -> FastAPI:
+    templates = Jinja2Templates(directory=Path(__file__).parent / 'templates')
+    static = StaticFiles(directory=Path(__file__).parent / 'static')
 
+    app = FastAPI()
+    app.mount('/static', static, name='static')
+    wiz = WizManager(
+        broadcast=config.broadcast,
+        timeout=config.timeout,
+        aliases=config.devices,
+    )
 
-templates = Jinja2Templates(directory=Path(__file__).parent.joinpath('templates'))
-app = FastAPI()
-app.mount('/static', StaticFiles(directory=Path(__file__).parent.joinpath('static')), name='static')
+    @app.get('/', response_class=HTMLResponse)
+    async def index(request: Request) -> Response:
+        data = {'page': 'Home page'}
+        return templates.TemplateResponse('index.j2', {'request': request, 'data': data})
 
+    @app.get('/lights', response_class=HTMLResponse)
+    async def get_lights(request: Request) -> Response:
+        res = []
 
-@app.get('/', response_class=HTMLResponse)
-async def index(request: Request) -> Response:
-    data = {'page': 'Home page'}
-    return templates.TemplateResponse('index.j2', {'request': request, 'data': data})
+        lights = await wiz.get_lights()
+        for light in lights:
+            await light.updateState()
+            name = wiz._aliases.get(light.mac) or light.mac
+            state = light.state.get_state()
+            res.append(
+                {
+                    'name': name,
+                    'ip': light.ip,
+                    'mac': light.mac,
+                    'status': state,
+                    'brightness': light.state.get_brightness() if state else 0,
+                    'colortemp': light.state.get_colortemp(),
+                }
+            )
 
+        return templates.TemplateResponse('lights.j2', {'request': request, 'lights': res})
 
-@app.get('/lights', response_class=HTMLResponse)
-async def get_lights(request: Request) -> Response:
-    res = []
+    @app.get('/lights/reset', response_class=HTMLResponse)
+    async def reset_lights(request: Request) -> Response:
+        wiz._lights = []
+        return Response()
 
-    lights = await _get_lights()
-    for light in lights:
+    @app.get('/lights/off', response_class=HTMLResponse)
+    async def lights_off(request: Request) -> Response:
+        for light in await wiz.get_lights():
+            await light.turn_off()
+        return await get_lights(request)
+
+    @app.get('/lights/on', response_class=HTMLResponse)
+    async def lights_on(request: Request) -> Response:
+        for light in await wiz.get_lights():
+            await light.turn_on()
+        return await get_lights(request)
+
+    @app.get('/lights/{name}/flip', response_class=HTMLResponse)
+    async def light_flip(request: Request) -> Response:
+        import pywizlight as wl
+
+        name = request.path_params['name']
+
+        light = wl.wizlight(name)
+        await light.lightSwitch()
         await light.updateState()
-        name = KNOWN_BULBS.get(light.mac) or light.mac
-        state = light.state.get_state()
-        res.append(
-            {
-                'name': name,
-                'ip': light.ip,
-                'mac': light.mac,
-                'status': state,
-                'brightness': light.state.get_brightness() if state else 0,
-                'colortemp': light.state.get_colortemp(),
-            }
-        )
+        return Response('ON') if light.state.get_state() else Response('OFF')
 
-    return templates.TemplateResponse('lights.j2', {'request': request, 'lights': res})
+    @app.post('/lights/{name}/brightness', response_class=HTMLResponse)
+    async def light_brightness(request: Request) -> None:
+        name = request.path_params['name']
+        value = int((await request.form())['value'])  # type: ignore[arg-type]
 
+        light = wl.wizlight(name)
+        await light.updateState()
 
-@app.get('/lights/reset', response_class=HTMLResponse)
-async def reset_lights(request: Request) -> Response:
-    global _discovered_lights
-    _discovered_lights = []
-    return Response()
+        if value:
+            await asyncio.wait_for(light.turn_on(wl.PilotBuilder(brightness=int(value))), config.timeout)
+        else:
+            await asyncio.wait_for(light.turn_off(), config.timeout)
 
+    @app.post('/lights/{name}/warmth', response_class=HTMLResponse)
+    async def light_warmth(request: Request) -> None:
+        name = request.path_params['name']
+        value = (await request.form())['value']
 
-@app.get('/lights/off', response_class=HTMLResponse)
-async def lights_off(request: Request) -> Response:
-    for light in await _get_lights():
-        await light.turn_off()
-    return await get_lights(request)
+        light = wl.wizlight(name)
+        await light.updateState()
+        timeout = config.timeout
+        await asyncio.wait_for(light.turn_on(wl.PilotBuilder(colortemp=int(value))), timeout)  # type: ignore[arg-type]
 
-
-@app.get('/lights/on', response_class=HTMLResponse)
-async def lights_on(request: Request) -> Response:
-    for light in await _get_lights():
-        await light.turn_on()
-    return await get_lights(request)
-
-
-@app.get('/lights/{name}/flip', response_class=HTMLResponse)
-async def light_flip(request: Request) -> Response:
-    import pywizlight as wl
-
-    name = request.path_params['name']
-
-    light = wl.wizlight(name)
-    await light.lightSwitch()
-    await light.updateState()
-    return Response('ON') if light.state.get_state() else Response('OFF')
-
-
-@app.post('/lights/{name}/brightness', response_class=HTMLResponse)
-async def light_brightness(request: Request) -> None:
-    name = request.path_params['name']
-    value = int((await request.form())['value'])  # type: ignore[arg-type]
-
-    light = wl.wizlight(name)
-    await light.updateState()
-    timeout = config.timeout
-
-    if value:
-        await asyncio.wait_for(light.turn_on(wl.PilotBuilder(brightness=int(value))), timeout)
-    else:
-        await asyncio.wait_for(light.turn_off(), timeout)
-
-
-@app.post('/lights/{name}/warmth', response_class=HTMLResponse)
-async def light_warmth(request: Request) -> None:
-    name = request.path_params['name']
-    value = (await request.form())['value']
-
-    light = wl.wizlight(name)
-    await light.updateState()
-    timeout = config.timeout
-    await asyncio.wait_for(light.turn_on(wl.PilotBuilder(colortemp=int(value))), timeout)  # type: ignore[arg-type]
+    return app
